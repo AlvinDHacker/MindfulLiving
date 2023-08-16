350aec3480c5f1c2e6f2896f0fe1983b62916f5e
Tribus <products@tribus.app>
2023-08-16 14:07:09 +0530

    Added Clarity

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { useEffect } from 'react';
 
 export default function Document() {
+
+  useEffect(() => {
+    (function (c, l, a, r, i, t, y) {
+      c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+      t = l.createElement(r); t.async = 1;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "ig1hhuwm9l");
+  }, []);
+
   return (
     <Html lang="en">
       <Head>
