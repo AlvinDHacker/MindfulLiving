8fead5dbc2152acb8f508b504b79e9bc0059dfdf
Tribus <products@tribus.app>
2023-08-16 14:17:40 +0530

    Added Clarity 2

--- pages/_document.js
+++ pages/_document.js
@@ -3,21 +3,18 @@ import { useEffect } from 'react';
 
 export default function Document() {
 
-  useEffect(() => {
-    (function (c, l, a, r, i, t, y) {
-      c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-      t = l.createElement(r); t.async = 1;
-      t.src = "https://www.clarity.ms/tag/" + i;
-      y = l.getElementsByTagName(r)[0];
-      y.parentNode.insertBefore(t, y);
-    })(window, document, "clarity", "script", "ig1hhuwm9l");
-  }, []);
-
   return (
     <Html lang="en">
       <Head>
         <title>Tech Tietans</title>
         <script defer src="https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.3/particles.min.js"></script>
+        <script defer type="text/javascript">
+          {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "ig1hhuwm9l");`}
+        </script>
       </Head>
       <body>
         <Main />
